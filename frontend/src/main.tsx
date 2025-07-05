68cee04eb94e9d8dcb6f5c15ec9d66d412376806
Zhaoyang Hu <1956708120@qq.com>
2025-07-05 12:51:03 +0800

    merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import CourseDetail from './CourseDetail';
-import './index.css';
-import CourseManagement from './CourseManagement'; 
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/course/:id" element={<CourseDetail />} />
-        <Route path="/manage" element={<CourseManagement />} />  {/* 新增管理页 */}
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -44,4 +22,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
->>>>>>> 56873210bdceee95aed3cde7b49554c5fcc480a8
